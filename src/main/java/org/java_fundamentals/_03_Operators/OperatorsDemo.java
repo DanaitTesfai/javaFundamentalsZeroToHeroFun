aa0cbaf319836c5e10401c0da4fc11e71841e326
Danait Tesfai <dtesfai2020@gmail.com>
2025-08-03 09:17:21 -0700

    OperatorsDemo:Arithmetic and comparison

---
+++ src/main/java/org/java_fundamentals/_03_Operators/OperatorsDemo.java
@@ -0,0 +1,29 @@
+package org.java_fundamentals._03_Operators;
+
+public class OperatorsDemo {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 8;
+
+        int additionResult = a + b;
+        System.out.println("Addition: " + additionResult);
+
+        int multiplicationResult = a * b;
+        System.out.println("Multiplication: " + multiplicationResult);
+
+        int remainderResult = b % a;
+        System.out.println("Remainder: " + remainderResult);
+
+        boolean com1 = a > 2;
+        boolean com2 = b > 16;
+        System.out.println("Comparison 1: " + com1);
+        System.out.println("Comparison 2: " + com2);
+
+
+
+
+
+
+    }
+
+}
