22030098974dcd8ecd2d675640977673e6b61e6a
Danait Tesfai <dtesfai2020@gmail.com>
2025-08-24 08:01:26 -0700

    Average of an array using for loop

---
+++ src/main/java/org/java_fundamentals/_06_arrays/_03_AverageOfArray.java
@@ -0,0 +1,17 @@
+package org.java_fundamentals._06_arrays;
+
+public class _03_AverageOfArray {
+    public static void main(String[] args) {
+        int [] numbers = {2, 4, 6, 8, 12};
+
+        int sum = 0;
+        double average = 0;
+        for (int i = 0 ; i < numbers.length ; i++) {
+            sum += numbers[i];
+
+        }
+        average = (double)sum / numbers.length;
+        System.out.println(average);
+
+    }
+}
