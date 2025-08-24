a0e05cbb23b9bf6bfeb0c9e0f6ff353c6d7c8ca8
Danait Tesfai <dtesfai2020@gmail.com>
2025-08-24 12:15:32 -0700

    Sum of digits using while loop

---
+++ src/main/java/org/java_fundamentals/_05_looping/_01_coding_challenges/_12_SumOfDigits.java
@@ -0,0 +1,23 @@
+package org.java_fundamentals._05_looping._01_coding_challenges;
+
+import java.util.Scanner;
+
+public class _12_SumOfDigits {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        int originalNumber = number ;
+        int sum = 0 ;
+        while(number > 0) {
+            int digit = number % 10 ;
+            sum += digit;
+            number /= 10;
+            System.out.print(digit + " + ");
+        }
+        System.out.println(" = " + sum);
+
+    }
+}
