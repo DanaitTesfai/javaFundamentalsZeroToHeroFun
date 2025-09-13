97f016d0cbeb83c1c37f13806d4cfa000df0a1b9
Danait Tesfai <dtesfai2020@gmail.com>
2025-09-13 09:18:11 -0700

    class and object bank account

--- src/main/java/org/java_fundamentals/_07_OOP/BankAccount.java
+++ src/main/java/org/java_fundamentals/_07_OOP/BankAccount.java
@@ -1,4 +1,48 @@
 package org.java_fundamentals._07_OOP;
 
 public class BankAccount {
+    String name;
+    String accountNumber;
+    double balance;
+
+    public void deposit(double amount){
+        balance += amount;
+        System.out.println("Deposited $" + amount + " successfully.");
+    }
+
+    public void withdraw(double amount){
+        if (balance < amount){
+            System.out.println("Withdrawal of $" + amount + " failed.");
+            System.out.println("Insufficient balance.");
+            return;
+        }
+        balance -= amount;
+        System.out.println("Withdrawal of $" + amount + " successful.");
+
+        /*
+             if (balance < amount){
+                System.out.println("Withdrawal of $" + amount + " failed.");
+                System.out.println("Insufficient balance.");
+
+             } else {
+                balance -= amount;
+                System.out.println("Withdrawal of $" + amount + " successful.");
+             }
+
+         */
+    }
+
+    public void checkBalance(){
+        System.out.println("Current balance: $" + balance);
+    }
+
+    public void displayAccountInfo(){
+        System.out.println("Account Info:");
+        System.out.println("Name: " + name);
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Balance: $" + balance);
+    }
+
+
+
 }
