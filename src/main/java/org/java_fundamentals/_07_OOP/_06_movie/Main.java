45cd0e8d41875986f498d91681a92f9aea5616f6
Danait Tesfai <dtesfai2020@gmail.com>
2025-09-13 12:06:16 -0700

    class and objects movies

---
+++ src/main/java/org/java_fundamentals/_07_OOP/_06_movie/Main.java
@@ -0,0 +1,17 @@
+package org.java_fundamentals._07_OOP._06_movie;
+
+public class Main {
+    public static void main(String[] args) {
+        Movie movie = new Movie();
+
+        movie.title = "Inception";
+        movie.genre = "Sci-Fi";
+        movie.duration = 148;
+        movie.rating = 8.8;
+
+        movie.playMovie();
+        movie.pauseMovie();
+        movie.stopMovie();
+        movie.displayMovieInfo();
+    }
+}
---
+++ src/main/java/org/java_fundamentals/_07_OOP/_06_movie/Movie.java
@@ -0,0 +1,28 @@
+package org.java_fundamentals._07_OOP._06_movie;
+
+public class Movie {
+    String title;
+    String genre;
+    int duration;
+    double rating;
+
+    public void playMovie(){
+        System.out.println("Playing movie: " + title);
+    }
+
+    public void pauseMovie(){
+        System.out.println("Movie " + title + " is paused.");
+    }
+
+    public void stopMovie(){
+        System.out.println("Movie " + title + " is stopped.");
+    }
+
+    public void displayMovieInfo(){
+        System.out.println("Movie info:");
+        System.out.println("Movie title: " + title);
+        System.out.println("Movie genre: " + genre);
+        System.out.println("Movie duration: " + duration + " minutes");
+        System.out.println("Movie rating: " + rating);
+    }
+}
