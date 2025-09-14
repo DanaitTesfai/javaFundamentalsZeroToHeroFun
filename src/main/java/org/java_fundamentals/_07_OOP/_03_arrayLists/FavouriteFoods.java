2bdeec386f1608b1f50c55ff130e08846223ea74
Danait Tesfai <dtesfai2020@gmail.com>
2025-09-14 12:45:50 -0700

    Arraylists food

---
+++ src/main/java/org/java_fundamentals/_07_OOP/_03_arrayLists/FavouriteFoods.java
@@ -0,0 +1,33 @@
+package org.java_fundamentals._07_OOP._03_arrayLists;
+
+import java.util.ArrayList;
+
+public class FavouriteFoods {
+    public static void main(String[] args) {
+        ArrayList <String> foods = new ArrayList<>();
+        foods.add("pizza");
+        foods.add("pasta");
+        foods.add("burger");
+
+        System.out.println(foods);
+        boolean isRemoved = foods.remove("pasta");
+        if (isRemoved){
+            System.out.println("Item removed successfully.");
+        }else {
+            System.out.println("Item cant be found.");
+        }
+        System.out.println(foods);
+
+
+        isRemoved = foods.remove("pasta");
+        if (isRemoved){
+            System.out.println("Item removed successfully.");
+        }else {
+            System.out.println("Item cant be found.");
+        }
+        System.out.println(foods);
+
+
+
+    }
+}
