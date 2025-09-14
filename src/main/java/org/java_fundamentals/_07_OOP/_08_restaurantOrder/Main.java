608ff157116c0c38cfbaf0548de1afd60daa331e
Danait Tesfai <dtesfai2020@gmail.com>
2025-09-13 20:18:43 -0700

    class and objects RestaurantOrder

---
+++ src/main/java/org/java_fundamentals/_07_OOP/_08_restaurantOrder/Main.java
@@ -0,0 +1,16 @@
+package org.java_fundamentals._07_OOP._08_restaurantOrder;
+
+public class Main {
+    public static void main(String[] args) {
+        RestaurantOrder restaurantOrder = new RestaurantOrder();
+
+        restaurantOrder.orderId = "O123";
+        restaurantOrder.customerName = "Alice";
+        restaurantOrder.foodItem = "Burger";
+        restaurantOrder.price = 8.5;
+
+        restaurantOrder.placeOrder();
+        restaurantOrder.cancelOrder();
+        restaurantOrder.displayOrderInfo();
+    }
+}
---
+++ src/main/java/org/java_fundamentals/_07_OOP/_08_restaurantOrder/RestaurantOrder.java
@@ -0,0 +1,24 @@
+package org.java_fundamentals._07_OOP._08_restaurantOrder;
+
+public class RestaurantOrder {
+    String orderId;
+    String customerName;
+    String foodItem;
+    Double price;
+
+    public void placeOrder(){
+        System.out.println("Order " + orderId + " placed for " + customerName);
+    }
+
+    public void cancelOrder(){
+        System.out.println("Order " + orderId + " has been canceled.");
+    }
+
+    public void displayOrderInfo(){
+        System.out.println("Order Info:");
+        System.out.println("ID: " + orderId);
+        System.out.println("Customer: " + customerName);
+        System.out.println("Food Item: " + foodItem);
+        System.out.println("Price: $" + price);
+    }
+}
