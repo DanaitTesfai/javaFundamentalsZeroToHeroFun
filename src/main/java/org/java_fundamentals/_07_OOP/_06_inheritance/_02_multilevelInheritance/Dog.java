d0c2ba1e30c6a325bf9cc0adfc02814417e4d998
Danait Tesfai <dtesfai2020@gmail.com>
2025-10-05 08:06:43 -0700

    Multilevel inheritance Animal-Mammal-Dog

---
+++ src/main/java/org/java_fundamentals/_07_OOP/_06_inheritance/_02_multilevelInheritance/Dog.java
@@ -0,0 +1,13 @@
+package org.java_fundamentals._07_OOP._06_inheritance._02_multilevelInheritance;
+
+public class Dog extends Mammal{
+    public void bark(){
+        System.out.println("Dogs can bark.");
+    }
+
+    @Override
+    public void eat() {
+        super.eat();
+        System.out.println("Dog is eating dog food.");
+    }
+}
---
+++ src/main/java/org/java_fundamentals/_07_OOP/_06_inheritance/_02_multilevelInheritance/Main.java
@@ -0,0 +1,10 @@
+package org.java_fundamentals._07_OOP._06_inheritance._02_multilevelInheritance;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.eat();
+        dog.walk();
+        dog.bark();
+    }
+}
