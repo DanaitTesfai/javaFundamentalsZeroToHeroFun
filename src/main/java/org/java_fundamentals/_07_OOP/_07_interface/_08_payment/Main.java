8afeee369e3d53c672902ffe62c56ed1287fb9ab
Danait Tesfai <dtesfai2020@gmail.com>
2025-10-18 08:04:02 -0700

    Refactor main menu

--- src/main/java/org/java_fundamentals/_07_OOP/_07_interface/_08_payment/Main.java
+++ src/main/java/org/java_fundamentals/_07_OOP/_07_interface/_08_payment/Main.java
@@ -1,10 +1,15 @@
 package org.java_fundamentals._07_OOP._07_interface._08_payment;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+    static ArrayList<String> menu =
+            new ArrayList<>(Arrays.asList("1\uFE0F⃣ UPI","2\uFE0F⃣ Debit/Credit Card","3\uFE0F⃣ Mobile Wallet","4\uFE0F⃣ Net Banking"));
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         
         while (true){
             System.out.println("1.Pay");
@@ -17,14 +22,11 @@ public class Main {
                 double amount = scanner.nextDouble();
                 if (amount <= 0){
                     System.out.println("❌ Invalid amount. Please enter a positive amount.");
-                    return;
+                    continue;
                 }
 
                 System.out.println("Select Payment Method:");
-                System.out.println("1\uFE0F⃣ UPI\n" +
-                        "2\uFE0F⃣ Debit/Credit Card\n" +
-                        "3\uFE0F⃣ Mobile Wallet\n" +
-                        "4\uFE0F⃣ Net Banking");
+                displayMenu(menu);
                 System.out.print("Enter choice: ");
                 int option = scanner.nextInt();
 
@@ -55,5 +57,10 @@ public class Main {
         
 
 
+    }
+    public static void displayMenu(ArrayList <String> menu){
+        for (String m : menu){
+            System.out.println(m);
+        }
     }
 }
