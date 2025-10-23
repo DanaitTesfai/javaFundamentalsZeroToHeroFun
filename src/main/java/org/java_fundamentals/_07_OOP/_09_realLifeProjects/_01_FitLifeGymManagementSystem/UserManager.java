7b64a7f4e53242e488e155383d61360bc6bf2f66
Danait Tesfai <dtesfai2020@gmail.com>
2025-10-23 05:16:09 -0700

    Fitlife gym UserManager

--- src/main/java/org/java_fundamentals/_07_OOP/_09_realLifeProjects/_01_FitLifeGymManagementSystem/UserManager.java
+++ src/main/java/org/java_fundamentals/_07_OOP/_09_realLifeProjects/_01_FitLifeGymManagementSystem/UserManager.java
@@ -3,28 +3,58 @@ package org.java_fundamentals._07_OOP._09_realLifeProjects._01_FitLifeGymManagem
 import java.util.ArrayList;
 
 public class UserManager {
-    ArrayList<UserOne> users = new ArrayList<>();
+    ArrayList<User> users = new ArrayList<>();
 
-    public void userManager(){
+    public UserManager(){
+        users.add(new User("admin","1234","Admin",true));
+        users.add(new User("staff1","pass","Staff",true));
+    }
+
+    public User authenticate(String username, String password){
+        for (User u: users){
+            if (u.getUsername().equalsIgnoreCase(username) && u.getPassword().equals(password) && u.isActive()){
+                return u;
+            }
+        }
+        return null;
 
     }
 
 
-    public String  userAuthenticate(){
+    public boolean addUser(User user){
+        if (users.contains(user)){
+            System.out.println("User already exists.");
+            return false;
+        }else {
+            users.add(user);
+            return true;
+        }
 
     }
 
-    public boolean addUser(){
+
+
+    public boolean deactivateUser(String username){
+        for (User u: users){
+            if (u.getUsername().equalsIgnoreCase(username)){
+                u.setActive(false);
+                return true;
+            }
+        }
+        return false;
 
     }
 
 
-    public boolean deactivateUser(){
 
-    }
-
-
-    public void void listUsers(){
+    public void listUsers(){
+        if (users.isEmpty()){
+            System.out.println("No user found");
+        }else {
+            for (User u: users){
+                u.displayInfo();
+            }
+        }
 
     }
 }
