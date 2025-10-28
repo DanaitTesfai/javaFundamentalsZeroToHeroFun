7f80f0ca673579125c57121a39d94088df1c4193
Danait Tesfai <dtesfai2020@gmail.com>
2025-10-28 01:12:29 -0700

    RoyalStay hotel UserManager

---
+++ src/main/java/org/java_fundamentals/_07_OOP/_09_realLifeProjects/_02_royalStayHotel/UserManager.java
@@ -0,0 +1,67 @@
+package org.java_fundamentals._07_OOP._09_realLifeProjects._02_royalStayHotel;
+
+import java.util.ArrayList;
+
+public class UserManager {
+    ArrayList<User> users = new ArrayList<>();
+
+
+    public UserManager(){
+        users.add(new User("admin", "1234", "Admin", true));
+    }
+
+
+
+
+
+    public User authenticate(String username, String password){
+        for (User u: users){
+            if (u.getUsername().equalsIgnoreCase(username) && u.getPassword().equals(password) && u.isActive()){
+                return u;
+            }
+        }
+        return null;
+    }
+
+
+
+
+
+    public boolean addUser(User user){
+        if (users.contains(user)){
+            System.out.println("User already exists.");
+            return false;
+        }else {
+            users.add(user);
+            return true;
+        }
+    }
+
+
+
+
+    public boolean deactivateUser(String username){
+        for (User u: users){
+            if (u.getUsername().equalsIgnoreCase(username)){
+                u.setActive(false);
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
+
+    public void listUsers(){
+        if (users.isEmpty()){
+            System.out.println("There are no users.");
+        }else {
+            for (User u: users){
+                u.displayInfo();
+            }
+        }
+    }
+
+
+}
