c7557c627386e7ceb31b9905e689ccd56fdf71c5
Danait Tesfai <dtesfai2020@gmail.com>
2025-11-15 09:31:49 -0800

    ReadFile

---
+++ src/main/java/org/java_fundamentals/_10_fileHandling/_02_bufferedApi/ReadMultipleLines.java
@@ -0,0 +1,21 @@
+package org.java_fundamentals._10_fileHandling._02_bufferedApi;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadMultipleLines {
+    public static void main(String[] args) {
+        try(BufferedReader reader = new BufferedReader(new FileReader("books.txt"))){
+            String line;
+
+            while ((line = reader.readLine()) != null){
+                System.out.println(line);
+            }
+
+
+        }catch (IOException  e){
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
