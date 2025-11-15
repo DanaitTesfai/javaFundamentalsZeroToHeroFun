9a165ed71a6607c8bfd53883db4d6f29668be95f
Danait Tesfai <dtesfai2020@gmail.com>
2025-11-15 12:47:25 -0800

    Replaceing a word in a file through buffering

---
+++ src/main/java/org/java_fundamentals/_10_fileHandling/_02_bufferedApi/ReplacingWord.java
@@ -0,0 +1,31 @@
+package org.java_fundamentals._10_fileHandling._02_bufferedApi;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class ReplacingWord {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a word to be replaced: ");
+        String oldWord = sc.nextLine();
+
+        System.out.print("Enter the new word: ");
+        String newWord = sc.nextLine();
+
+        try (
+                BufferedReader reader = new BufferedReader(new FileReader("lesson.txt"));
+                BufferedWriter writer = new BufferedWriter(new FileWriter("edited_lesson.txt"))
+        ) {
+            String text;
+            while((text = reader.readLine())!= null){
+                writer.write(text.replace(oldWord,newWord));
+                writer.newLine();
+            }
+            System.out.println("Word replaced successfully. Output: edited_lesson.txt");
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
