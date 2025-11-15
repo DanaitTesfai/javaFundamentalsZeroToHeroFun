de1f2dd086545c5b423ade9a7575655bf1c2ea69
Danait Tesfai <dtesfai2020@gmail.com>
2025-11-15 12:10:52 -0800

    Removing empty lines through buffering

---
+++ src/main/java/org/java_fundamentals/_10_fileHandling/_02_bufferedApi/RemoveEmptyLines.java
@@ -0,0 +1,25 @@
+package org.java_fundamentals._10_fileHandling._02_bufferedApi;
+
+import java.io.*;
+
+public class RemoveEmptyLines {
+    public static void main(String[] args) {
+        try(
+                BufferedReader reader = new BufferedReader(new FileReader("raw_text.txt"));
+                BufferedWriter writer = new BufferedWriter(new FileWriter("clean_text.txt"));
+        ){
+
+            String text;
+            while ((text = reader.readLine()) != null){
+                if (!text.trim().isEmpty()){
+                    writer.write(text.trim());
+                    writer.newLine();
+                }
+            }
+            System.out.println("Empty lines removed. Output: clean_text.txt");
+
+        }catch (IOException e){
+            System.out.println("Error" + e.getMessage());
+        }
+    }
+}
