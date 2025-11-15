c87c6bf7837f189446c9677c6e0c4eb213e3d1a7
Danait Tesfai <dtesfai2020@gmail.com>
2025-11-15 09:53:24 -0800

    Copying a file through Buffering

---
+++ src/main/java/org/java_fundamentals/_10_fileHandling/_02_bufferedApi/CopyBufferedReaderWriter.java
@@ -0,0 +1,21 @@
+package org.java_fundamentals._10_fileHandling._02_bufferedApi;
+
+import java.io.*;
+
+public class CopyBufferedReaderWriter {
+    public static void main(String[] args) {
+        try(
+                BufferedReader reader = new BufferedReader(new FileReader("poem.txt"));
+                BufferedWriter writer = new BufferedWriter(new FileWriter("poem_backup.txt"));
+        ){
+            String line;
+            while ((line = reader.readLine())!= null){
+                writer.write(line);
+                writer.newLine();
+            }
+
+        }catch (IOException e){
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
