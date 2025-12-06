c4bb2e5616b61d62d2689e4e8c799c686ae2e5a0
Danait Tesfai <dtesfai2020@gmail.com>
2025-12-06 08:43:53 -0800

    Data structures-Set  Remove elements

---
+++ src/main/java/org/java_fundamentals/_12_dataStructures/_06_RemoveElements.java
@@ -0,0 +1,20 @@
+package org.java_fundamentals._12_dataStructures;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class _06_RemoveElements {
+    public static void main(String[] args) {
+        Set<Integer> ints = new HashSet<>();
+
+        ints.add(5);
+        ints.add(10);
+        ints.add(30);
+        ints.add(45);
+        ints.add(55);
+        ints.add(65);
+
+        ints.removeIf(n -> n > 50);
+        System.out.println(ints);
+    }
+}
