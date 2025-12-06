1f8247e86741af068fef322ab03f3b397336b1b4
Danait Tesfai <dtesfai2020@gmail.com>
2025-12-06 08:50:18 -0800

    Data structures-Set  Convert from set to list

---
+++ src/main/java/org/java_fundamentals/_12_dataStructures/_07_ConvertSetToList.java
@@ -0,0 +1,21 @@
+package org.java_fundamentals._12_dataStructures;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class _07_ConvertSetToList {
+    public static void main(String[] args) {
+        Set<String> coloursSet = new HashSet<>();
+
+        coloursSet.add("Red");
+        coloursSet.add("Green");
+        coloursSet.add("Blue");
+
+        List<String> coloursList = new ArrayList<>(coloursSet);
+
+        System.out.println(coloursList);
+
+    }
+}
