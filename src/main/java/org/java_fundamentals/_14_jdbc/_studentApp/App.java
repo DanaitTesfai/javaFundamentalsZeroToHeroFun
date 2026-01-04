7c7f4b489aa52a5bbc383b6c3a585b01be402dd0
Danait Tesfai <dtesfai2020@gmail.com>
2026-01-04 14:02:43 -0800

    jdbc StudentSchool app complete

--- src/main/java/org/java_fundamentals/_14_jdbc/_studentApp/App.java
+++ src/main/java/org/java_fundamentals/_14_jdbc/_studentApp/App.java
@@ -1,24 +1,10 @@
 package org.java_fundamentals._14_jdbc._studentApp;
 
-import org.java_fundamentals._14_jdbc._studentApp.model.Student;
-import org.java_fundamentals._14_jdbc._studentApp.service.StudentServiceImpl;
-
-import java.io.IOException;
-import java.sql.SQLException;
-import java.sql.Timestamp;
+import org.java_fundamentals._14_jdbc._studentApp.view.MainMenu;
 
 public class App {
-    public static void main(String[] args) throws SQLException, IOException {
-
-        StudentServiceImpl studentService = new StudentServiceImpl();
-        Timestamp now = new Timestamp(System.currentTimeMillis());
-        Student student = new Student("Alice", "Alice@gmail.com", "23456", now.toLocalDateTime());
-        boolean isAdded = studentService.addStudent(student);
-        if (isAdded){
-            System.out.println("Student added successfully.");
-        }else {
-            System.out.println("insert failed.");
-        }
+    public static void main(String[] args)  {
+        new MainMenu().start();
 
     }
 }
---
+++ src/main/java/org/java_fundamentals/_14_jdbc/_studentApp/view/MainMenu.java
@@ -0,0 +1,113 @@
+package org.java_fundamentals._14_jdbc._studentApp.view;
+
+import org.java_fundamentals._14_jdbc._studentApp.controller.StudentController;
+import org.java_fundamentals._14_jdbc._studentApp.model.Student;
+
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
+
+public class MainMenu {
+    StudentController studentController = new StudentController();
+    Scanner sc = new Scanner(System.in);
+
+    public void start(){
+        while (true){
+            printMenu();
+            int choice = sc.nextInt();
+            sc.nextLine();
+
+            switch (choice) {
+                case 1 -> addStudentUI();
+                case 2 -> viewAllUI();
+                case 3 -> findByIdUI();
+                case 4 -> updateUI();
+                case 5 -> deleteUI();
+                case 0 -> {
+                    System.out.println("Bye!");
+                    return;
+                }
+                default -> System.out.println("Invalid choice!");
+            }
+        }
+    }
+
+    private void deleteUI() {
+        System.out.println("Id: ");
+        int id = sc.nextInt();
+        sc.nextLine();
+
+        boolean isDeleted = studentController.deleteStudentById(id);
+        System.out.println(isDeleted ? "Student deleted." : "Student deleting failed.");
+    }
+
+    private void updateUI() {
+        System.out.println("Id: ");
+        int id = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Name: ");
+        String name = sc.nextLine().trim();
+
+        System.out.print("Email: ");
+        String email = sc.nextLine().trim();
+
+        System.out.print("Phone: ");
+        String phone = sc.nextLine().trim();
+
+        boolean isUpdated = studentController.updateStudent(id,name, email, phone);
+        System.out.println(isUpdated ? "Student updated." : "Student updating failed.");
+    }
+
+    private void findByIdUI() {
+        System.out.println("Id: ");
+        int id = sc.nextInt();
+        sc.nextLine();
+        Optional<Student> student = studentController.findStudentById(id);
+        if (student.isPresent()){
+            System.out.println(student.get());
+        }else {
+            System.out.println("Student not found.");
+        }
+    }
+
+    private void viewAllUI() {
+        List<Student> students = studentController.listStudents();
+        if (students.isEmpty()){
+            System.out.println("There are no students to display.");
+            return;
+        }
+        for (Student s : students) {
+            System.out.println(s);
+        }
+    }
+
+    private void addStudentUI() {
+        System.out.print("Name: ");
+        String name = sc.nextLine().trim();
+
+        System.out.print("Email: ");
+        String email = sc.nextLine().trim();
+
+        System.out.print("Phone: ");
+        String phone = sc.nextLine().trim();
+
+        Timestamp now = new Timestamp(System.currentTimeMillis());
+        Student student = new Student(name,email, phone, now.toLocalDateTime());
+        boolean isCreated = studentController.createStudent(student);
+        System.out.println(isCreated ? "✅ Student added." : "❌ Failed to add student.");
+
+    }
+
+
+    private void printMenu(){
+        System.out.println("\n===== Student Register =====");
+        System.out.println("1. Add Student");
+        System.out.println("2. View All Students");
+        System.out.println("3. Find Student By ID");
+        System.out.println("4. Update Student (Email/Phone)");
+        System.out.println("5. Delete Student");
+        System.out.println("0. Exit");
+    }
+}
